e4e9ed564cbedd0264966180c814026e4437f849
kamilKoo <kamiekurat98@o2.pl>
2022-07-21 15:54:44 +0200

    Created user module.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
       }),
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
