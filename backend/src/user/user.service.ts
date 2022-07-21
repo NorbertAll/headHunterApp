6a5298e983de50839474d9684429644ac7f40bc4
kamilKoo <kamiekurat98@o2.pl>
2022-07-21 17:33:04 +0200

    Added find, findOne, update, function in UserService.

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -12,4 +12,13 @@ export class UserService {
     async save(options) {
         return this.userRepository.save(options);
     }
+    async find(options) {
+        return this.userRepository.find(options);
+    }
+    async findOne(options) {
+        return this.userRepository.findOne(options);
+    }
+    async update(id: number, options){
+        return this.userRepository.update(id, options);
+    }
 }
