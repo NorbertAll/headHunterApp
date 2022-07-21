cced85930593d0c6caf185f33ca27f9c4d583e61
dLeczycki <danielleczycki@gmail.com>
2022-07-21 08:10:03 +0200

    Delete WebVitals

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,9 +4,8 @@ import { Provider } from 'react-redux';
 
 import '@fontsource/roboto';
 
-import { store } from './app/store';
+import { store } from './store/store';
 import { App } from './App';
-import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 
@@ -20,8 +19,3 @@ root.render(
     </Provider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
