21396d3f85acfddc7a312b4e235a8f2eeff14656
kamilKoo <kamiekurat98@o2.pl>
2022-07-21 17:13:04 +0200

    Imports User. Added save function in UserService.

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import { User } from './entities/user.entity';
 
 @Module({
+  imports:[
+      TypeOrmModule.forFeature([User])
+  ],
   controllers: [UserController],
   providers: [UserService]
 })
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {User} from "./entities/user.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User) private readonly userRepository: Repository<User>
+    ) {
+    }
+    async save(options) {
+        return this.userRepository.save(options);
+    }
+}
