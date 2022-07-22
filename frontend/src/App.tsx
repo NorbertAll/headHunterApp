2a15c60ec2c63ee0f6c0b10730a136f6e4e1b15a
dLeczycki <danielleczycki@gmail.com>
2022-07-22 08:23:26 +0200

    Add dark theme to application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,24 @@
 import { Provider } from 'react-redux';
-import { Testing } from 'types';
+
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+
+import { Router } from 'components/router/Router';
+
 import { store } from './store/store';
-import {Router} from "./components/router/Router";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export const App = () => {
-    // types from BE to FE testing
-    const typesTest: Testing = {
-        name: 'john',
-        age: 22,
-    };
   return (
     <Provider store={store}>
-      <Router />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Router />
+      </ThemeProvider>
     </Provider>
   );
 };
