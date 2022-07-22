5f7957a56a82ed2492d5c3cf8faf89f72e0b4170
dLeczycki <danielleczycki@gmail.com>
2022-07-22 13:22:44 +0200

    Add simple NotFound and Unauthorized pages

---
+++ frontend/src/components/global/NotFound.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+export const NotFound = () => {
+  return (
+    <Typography variant="h2">
+      Uuups.. strona nie została odnaleziona...
+    </Typography>
+  );
+};
---
+++ frontend/src/components/global/Unauthorized.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+export const Unauthorized = () => {
+  return (
+    <Typography variant="h2">
+      Nie masz uprawnień, aby odwiedzić tę stronę
+    </Typography>
+  );
+};
