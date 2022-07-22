d9c2325f3a4e4c4f4dcdc5e92fd643cbd251ea34
dLeczycki <danielleczycki@gmail.com>
2022-07-22 10:19:06 +0200

    Move theme to separate file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,14 @@ import { Provider } from 'react-redux';
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
 import { Router } from 'components/router/Router';
+import { theme } from 'utils/theme';
 
 import { store } from './store/store';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
 
 export const App = () => {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <Router />
       </ThemeProvider>
