7a2235e8601fa25d9adc9e325d8685f3a76f3cd4
dLeczycki <danielleczycki@gmail.com>
2022-07-22 11:56:48 +0200

    Remove Redux from project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,13 @@
-import { Provider } from 'react-redux';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
 import { Router } from 'components/router/Router';
 import { theme } from 'utils/theme';
 
-import { store } from './store/store';
-
 export const App = () => {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Router />
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router />
+    </ThemeProvider>
   );
 };
