48a09db61a3d31c39ca6aa86691f42fda2d4da25
dLeczycki <danielleczycki@gmail.com>
2022-07-22 13:24:35 +0200

    Add simple login functionality

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -1,8 +1,42 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Box, Button, Link, TextField, Typography, Stack } from '@mui/material';
+import { SyntheticEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Auth, UserRoles } from 'contexts/auth/AuthProvider';
+import { useAuth } from 'contexts/auth/useAuth';
+
+const sendLoginRequest = async (
+  email: string,
+  password: string,
+): Promise<Auth> => {
+  await setTimeout(() => {
+    console.log(email, password);
+  }, 2000);
+
+  return {
+    accessToken: '1244123asdas',
+    roles: [UserRoles.ADMIN, UserRoles.STUDENT, UserRoles.HR],
+  };
+};
 
 export const Login = () => {
-  const handleSubmit = () => {};
+  const { setAuth } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    try {
+      const loginResponse = await sendLoginRequest('test@email', 'password');
+      console.log(loginResponse);
+      setAuth(loginResponse);
+    } catch (error) {
+      console.log(error);
+    }
+
+    navigate('/admin');
+  };
 
   return (
     <Box
