e746821bedd3e2bb2716211ec8fd8d8dba386637
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-07-23 12:59:48 +0100

    Added component to routes.

--- frontend/src/components/router/Router.tsx
+++ frontend/src/components/router/Router.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { HeaderAppBar } from 'components/layout/Hr/HeaderAppBar/HeaderAppBar';
+
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Hello World</div>} />
+        {/* HeaderAppBar need add to routes protected for hr */}
+        <Route path="/hr" element={<HeaderAppBar />} />
+        <Route path="/hr/account" element={<div>Konto HR</div>} />
+        <Route path="/login" element={<div>Strona Logowania</div>} />
       </Routes>
     </BrowserRouter>
   );
