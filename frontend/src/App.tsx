5c8ccea70ebe9108170b73001789001268291c99
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-07-23 12:58:15 +0100

    Created basic theme.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import { Provider } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 import { Router } from 'components/router/Router';
+import { theme } from 'utils/theme';
 
 import { store } from './store/store';
 
 export const App = () => {
   return (
     <Provider store={store}>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router />
+      </ThemeProvider>
     </Provider>
   );
 };
