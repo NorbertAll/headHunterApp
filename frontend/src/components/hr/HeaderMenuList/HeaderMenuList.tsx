da63a7340f064b73adb96537a0425b488bc0ec70
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-07-23 12:59:14 +0100

    Added HeaderMenuList component.

---
+++ frontend/src/components/hr/HeaderMenuList/HeaderMenuList.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { Avatar, Menu, Typography } from '@mui/material';
+
+import {
+  StyledBoxInfoContainer,
+  StyledBoxMenuIcon,
+  StyledBoxUserName,
+  StyledMenuItem,
+  StyledMenuItemLink,
+} from 'components/hr/HeaderMenuList/HeaderMenuList.styles';
+
+export const HeaderMenuList = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <StyledBoxInfoContainer onClick={handleClick}>
+        <Avatar
+          alt="Tester Testowy"
+          src="https://github.com/lukaszzadamczyk.png"
+        />
+        <StyledBoxUserName>
+          <Typography>Tester Testowy</Typography>
+        </StyledBoxUserName>
+        <StyledBoxMenuIcon>
+          <ArrowDropDownIcon />
+        </StyledBoxMenuIcon>
+      </StyledBoxInfoContainer>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <StyledMenuItemLink to="/hr/account">
+          <StyledMenuItem onClick={handleClose}>Konto</StyledMenuItem>
+        </StyledMenuItemLink>
+        <StyledMenuItemLink to="/login">
+          <StyledMenuItem onClick={handleClose}>Wyloguj</StyledMenuItem>
+        </StyledMenuItemLink>
+      </Menu>
+    </>
+  );
+};
