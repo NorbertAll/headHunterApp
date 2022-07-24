3c9dc610e89f36fa87a32e852d924130d8271d4b
Radek Holubowski <radekh8787@gmail.com>
2022-07-24 17:25:44 +0100

    Refactored types configurations files.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import { Provider } from 'react-redux';
-
-import { Router } from 'components/router/Router';
-
+import { Testing } from 'types';
 import { store } from './store/store';
+import {Router} from "./components/router/Router";
 
 export const App = () => {
+    // types from BE to FE testing
+    const typesTest: Testing = {
+        name: 'john',
+        age: 22,
+    };
   return (
     <Provider store={store}>
       <Router />
