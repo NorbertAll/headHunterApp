d2516a5e3648bd3dbd3da728e481b9afd59298ca
NorbertAll <norbert_gutkowski@interia.pl>
2022-07-25 21:18:24 +0200

    fix conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,16 @@
 import { Provider } from 'react-redux';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
-import { Router } from './router/Router';
+
 import { theme } from './utils/theme';
 
 import { store } from './store/store';
-import {Router} from "./components/router/Router";
+import { Router } from './router/Router';
+
 
 export const App = () => {
     // types from BE to FE testing
-    const typesTest: Testing = {
-        name: 'john',
-        age: 22,
-    };
+  
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
