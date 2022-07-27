029b915a506a447ce0c06e6bdf3937d5a4356735
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-07-27 20:08:20 +0100

    Added theme colors.

--- frontend/src/utils/theme.ts
+++ frontend/src/utils/theme.ts
@@ -4,7 +4,18 @@ export const theme = createTheme({
   palette: {
     mode: 'dark',
     primary: {
-      main: '#d32f2f',
+      main: '#222225',
+      light: '#2a2a2b',
+      dark: '#1e1e20',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#0a0a0a',
+      light: '#9e9e9e',
+      contrastText: '#37c7d7d',
+    },
+    error: {
+      main: '#e02735',
     },
   },
 });
