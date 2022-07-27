caeadd64eea7b6575e14edaa45616ea030dc5d82
JasonDollar <elrey33@gmail.com>
2022-07-27 11:51:02 +0200

    Controlls section initial styling

---
+++ frontend/src/components/StudentsList/Search.tsx
@@ -0,0 +1,9 @@
+
+
+const Search = () => {
+  return (
+    <div>ADD search input</div>
+  )
+}
+
+export default Search
--- frontend/src/components/StudentsList/StudentsList.tsx
+++ frontend/src/components/StudentsList/StudentsList.tsx
@@ -3,14 +3,17 @@ import styled from '@emotion/styled';
 
 import c from '../../utils/constants';
 
+import Search from './Search';
+
 const Container = styled.div`
-  maxWidth: 1430px;
+  max-width: 1430px;
   background: #292A2B;
   margin: 1.625rem auto;
 `
 
 const Tabs = styled.div`
   display: flex;
+  border-bottom: 2px solid #202122;
 `;
 
 const Tab = styled.div<{ activeTab: boolean }>`
@@ -21,6 +24,25 @@ const Tab = styled.div<{ activeTab: boolean }>`
   border-bottom: ${(props) =>
     props.activeTab ? '3px solid #e02735' : '3px solid transparent'};
 `;
+
+const List = styled.div`
+  padding: 0 20px;
+`
+
+const Controlls = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 2px solid #202122;
+  padding: 20px 0;
+`
+
+const FilterButton = styled.button`
+  color: #f7f7f7;
+  font-size: 16px;
+  background-color: #1E1E1F;
+  border: none;
+  padding: 6px 10px;
+`
 
 const StudentsList = () => {
   const [activeTab, setActiveTab] = useState(c.INTERVIEWS);
@@ -45,7 +67,13 @@ const StudentsList = () => {
           Do rozmowy
         </Tab>
       </Tabs>
-      <div>StudentsList</div>
+      <List>
+        <Controlls>
+          <Search />
+          <FilterButton>Filtrowanie</FilterButton>
+        </Controlls>
+        LISTA
+      </List>
     </Container>
   );
 };
