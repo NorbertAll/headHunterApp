1121c542c6a213e99903c4609fc59d3cd9c2ddf2
dLeczycki <danielleczycki@gmail.com>
2022-07-27 08:20:07 +0200

    Enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
   })
   await app.listen(3001);
 }
