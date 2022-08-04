a30e2ec63bdf8cff950b37b514c1bff431d9036a
dLeczycki <danielleczycki@gmail.com>
2022-08-04 13:13:19 +0200

    Implement prettier requirements

--- backend/src/auth/auth.role.guard.ts
+++ backend/src/auth/auth.role.guard.ts
@@ -1,18 +1,18 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { ROLES_KEY } from './auth.role.dekorator';
+import { ROLES_KEY } from './auth.role.decorator';
 import { JwtService } from '@nestjs/jwt';
-import { UserRoles } from 'types';
+import { Role } from 'types';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector, private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const requiredRoles = this.reflector.getAllAndOverride<UserRoles[]>(
-      ROLES_KEY,
-      [context.getHandler(), context.getClass()],
-    );
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
 
     if (!requiredRoles) {
       return true;
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,21 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import {JwtModule} from "@nestjs/jwt";
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports:[
-      TypeOrmModule.forFeature([User]),
-      JwtModule.register({
-          secret: 'secret',
-          signOptions: {expiresIn: '1d'},
-      }),
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1d' },
+    }),
   ],
   controllers: [UserController],
   providers: [UserService],
-  exports: [UserService]
-
+  exports: [UserService],
 })
 export class UserModule {}
