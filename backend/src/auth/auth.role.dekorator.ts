16f2c6eccce51f63263967635da6da40f045bb7b
dLeczycki <danielleczycki@gmail.com>
2022-08-04 13:09:47 +0200

    Change auth.decorator file name

--- backend/src/auth/auth.role.dekorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { UserRoles } from 'types';
-
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: UserRoles[]) => SetMetadata('roles', roles);
