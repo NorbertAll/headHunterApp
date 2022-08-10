89edb79eb29d65361abe7379495dbc886f5f924d
JasonDollar <elrey33@gmail.com>
2022-08-10 20:38:56 +0200

    Subrow componwnt created

--- frontend/src/components/StudentsList/Row.tsx
+++ frontend/src/components/StudentsList/Row.tsx
@@ -1,7 +1,11 @@
-import { TableCell, TableRow , styled, Avatar, } from '@mui/material'
+import { useState } from 'react'
+import { TableCell, TableRow , styled, Avatar, IconButton } from '@mui/material'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 
 import c from '../../utils/constants';
 
+import Subrow from './Subrow'
 import { ActionButton } from './styles';
 import { IStudent } from './types';
 
@@ -47,33 +51,41 @@ interface IProps {
   activeTab: string
 }
 
-const Row = ({ student, index, activeTab, tabLength }: IProps) => {
-  console.log('rend')
+const Row = ({ student, index, activeTab, tabLength = 0 }: IProps) => {
+  const [ subrowOpen, setSubrowOpen ] = useState(false)
   return (
-    <StyledTableRow key={student.id_student} className={index === tabLength - 1 ? 'no-border-bottom' : ''}>
-      {activeTab === c.INTERVIEWS && (
-        <TableCell className="reservation">
-            <span className='reservation-tag'>Rezerwacja do</span>
-            <span className='date'>30.07.2022</span>
+    <>
+      <StyledTableRow key={student.id_student} className={index === tabLength - 1 ? 'no-border-bottom' : ''}>
+        {activeTab === c.INTERVIEWS && (
+          <TableCell className="reservation">
+              <span className='reservation-tag'>Rezerwacja do</span>
+              <span className='date'>30.07.2022</span>
+          </TableCell>
+        )}
+        {activeTab === c.INTERVIEWS && (
+          <Avatar />
+        )}
+        <TableCell className='max-width name'>
+        {activeTab === c.STUDENTS ? 
+        `${student.first_name} ${student.last_name[0]}.` :
+        `${student.first_name} ${student.last_name}`
+        }
         </TableCell>
-      )}
-      {activeTab === c.INTERVIEWS && (
-        <Avatar />
-      )}
-      <TableCell className='max-width name'>
-      {activeTab === c.STUDENTS ? 
-      `${student.first_name} ${student.last_name[0]}.` :
-      `${student.first_name} ${student.last_name}`
-      }
-      </TableCell>
-      <TableCell>
-        {activeTab === c.STUDENTS && <ActionButton className='m-l-sm'>Zarezerwuj rozmowę</ActionButton>}
-        {activeTab === c.INTERVIEWS && <ActionButton className='m-l-sm'>Pokaż CV</ActionButton>}
-        {activeTab === c.INTERVIEWS && <ActionButton className='m-l-sm'>Brak zainteresowania</ActionButton>}
-        {activeTab === c.INTERVIEWS && <ActionButton className='m-l-sm'>Zatrudniony</ActionButton>}
-        
-      </TableCell>
-    </StyledTableRow>
+        <TableCell>
+          {activeTab === c.STUDENTS && <ActionButton className='m-l-sm'>Zarezerwuj rozmowę</ActionButton>}
+          {activeTab === c.INTERVIEWS && <ActionButton className='m-l-sm'>Pokaż CV</ActionButton>}
+          {activeTab === c.INTERVIEWS && <ActionButton className='m-l-sm'>Brak zainteresowania</ActionButton>}
+          {activeTab === c.INTERVIEWS && <ActionButton className='m-l-sm'>Zatrudniony</ActionButton>}
+          
+        </TableCell>
+        <TableCell>
+          <IconButton aria-label="expand row" size="small" onClick={() => setSubrowOpen(prev => !prev)}>
+            {subrowOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+      </StyledTableRow>
+      <Subrow subrowOpen={subrowOpen} student={student}/>
+    </>
   )
 }
 
---
+++ frontend/src/components/StudentsList/Subrow.tsx
@@ -0,0 +1,115 @@
+import {
+  Collapse, TableBody, Box, TableCell, TableRow, styled
+} from '@mui/material'
+
+import { IStudent } from './types'
+// import { Link } from 'react-router-dom'
+
+interface IProps {
+  subrowOpen: boolean
+  student: IStudent
+}
+
+const StyledTableRow = styled(TableRow)(() => ({
+  background: '#222324',
+  '&:last-child td, &:last-child th': {
+    border: 'none',
+  },
+  '& td': { 
+    border: 'transparent',
+    padding: 16,
+  },
+  '& .info-container': { 
+    borderBottom: 'none', display: 'flex', padding: 0
+  },
+  '& .info': {
+    display: 'flex',
+    flexDirection: 'column',
+    borderRight: '1px solid #292A2B',
+    minHeight: 110,
+  },
+  '& .info:last-child': {
+    borderRight: 'none'
+  },
+  '& .header': {
+    color: '#CFCFCF',
+    fontSize: 12,
+    marginBottom: 'auto',
+  },
+  '& .grade': {
+    fontSize: 15,
+    color: '#F7F7F7'
+  },
+  '& .grading': {
+    fontSize: 15,
+    color: '#9E9E9E'
+  }
+}))
+
+const DetailUsageRow = ({ student, subrowOpen }: IProps) => {
+  return (
+    <StyledTableRow>
+      <TableCell style={{ padding: 0 }} colSpan={10}>
+        <Collapse in={subrowOpen} timeout="auto" unmountOnExit>
+          <Box>
+            {/* <Table size="small" aria-label="Requirement details"> */}
+              {/* <TableHead>
+                <TableRow>
+                  <TableCell>Process Order</TableCell>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Requirement</TableCell>
+                  <TableCell>Material</TableCell>
+                </TableRow>
+              </TableHead> */}
+              <TableBody className="info-container">
+                <TableCell className='info'>
+                  <p className="header">Ocena przejścia kursu</p>
+                  <p className="grading"><span className='grade'>{student.course_completion}</span>/5</p>
+                </TableCell>
+                <TableCell className='info'>
+                  <p className="header">Ocena aktywności i zaangażowania na kursie</p>
+                  <p className="grading"><span className='grade'>{student.course_completion}</span>/5</p>
+                </TableCell>
+                <TableCell className='info'>
+                  <p className="header">Ocena kodu w projekcie własnym</p>
+                  <p className="grading"><span className='grade'>{student.course_completion}</span>/5</p>
+                </TableCell>
+                <TableCell className='info'>
+                  <p className="header">Ocena pracy w zespole W Scrum</p>
+                  <p className="grading"><span className='grade'>{student.course_completion}</span>/5</p>
+                </TableCell>
+                <TableCell className='info'>
+                  <p className="header">Preferowane miejsce pracy</p>
+                  <p className="grading"><span className='grade'>{student.course_completion}</span>/5</p>
+                </TableCell>
+                <TableCell className='info'>
+                  <p className="header">Docelowe miejsce gdzie chce pracować kandydat</p>
+                  <p className="grading"><span className='grade'>{student.course_completion}</span>/5</p>
+                </TableCell>
+                <TableCell className='info'>
+                  <p className="header">Oczekiwany typ kontraktu</p>
+                  <p className="grading"><span className='grade'>{student.course_completion}</span>/5</p>
+                </TableCell>
+                <TableCell className='info'>
+                  <p className="header">Oczekiwane wynagrodzenie miesięczne netto</p>
+                  <p className="grading"><span className='grade'>{student.course_completion}</span>/5</p>
+                </TableCell>
+                <TableCell className='info'>
+                  <p className="header">Zgoda na odbycie bezpłatnych praktyk/ stażu na początek</p>
+                  <p className="grading"><span className='grade'>{student.course_completion}</span>/5</p>
+                </TableCell>
+                <TableCell className='info'>
+                  <p className="header">Komercyjne doświadczenie w programowaniu</p>
+                  <p className="grading"><span className='grade'>{student.course_completion}</span>/5</p>
+                </TableCell>
+              </TableBody>
+            {/* </Table> */}
+          </Box>
+        </Collapse>
+      </TableCell>
+    </StyledTableRow>
+  )
+}
+
+export default DetailUsageRow
+
