5ffb596710562661844ac824f891ceffaf03338e
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-08-11 13:03:24 +0200

    Added styles to comonent StudentDetails.

---
+++ frontend/src/components/StudentDetails/StudentDetails.styles.ts
@@ -0,0 +1,6 @@
+import { Container, styled } from '@mui/material';
+import { pxToRem } from '../../utils/styles';
+
+export const StyledContainerStudentDetails = styled(Container)(() => ({
+  margin: `${pxToRem(26)} auto`,
+}));
