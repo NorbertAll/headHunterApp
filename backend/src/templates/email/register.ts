4d71c4e9cc2810058e9d0d54ca2df67521274819
Emil <kamiekurat98@o2.pl>
2022-08-11 23:43:08 +0200

    Tested send email. Added templates email.

---
+++ backend/src/templates/email/register.ts
@@ -0,0 +1,10 @@
+export function registerTemplate(name, token) {
+  return `<p>Czesc ${name},</p>
+<p>Prosze o klikniecie w link do rejestracji</p>
+<p>
+ 
+  <p><a href="http://localhost:3000/registrationstudent">REJESTRACJA</a></p>
+</p>
+
+<p>Jesli nie chcesz odpowiedziec na ta wiadomosc to ja zignoruj.</p>`;
+}
