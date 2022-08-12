3a751519e3cb04bcef259a22d71bc5af3897adbd
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-08-12 14:53:28 +0200

    Fixed component StudentDetails.

--- frontend/src/components/StudentDetails/StudentDetails.tsx
+++ frontend/src/components/StudentDetails/StudentDetails.tsx
@@ -1,7 +1,8 @@
 import { Grid } from '@mui/material';
 import { HeaderAppBar } from '../HeaderAppBar/HeaderAppBar';
+import { StudentContainerInfo } from './StudentContainerInfo/StudentContainerInfo';
 import { StudentCardInfo } from './StudentCardInfo/StudentCardInfo';
-
+import { pxToRem } from '../../utils/styles';
 import { StyledContainerStudentDetails } from './StudentDetails.styles';
 
 export const StudentDetails = () => {
@@ -13,8 +14,8 @@ export const StudentDetails = () => {
           <Grid item xs={3}>
             <StudentCardInfo />
           </Grid>
-          <Grid item xs={9}>
-            sa
+          <Grid item xs={9} borderLeft={`${pxToRem(4)} solid transparent`}>
+            <StudentContainerInfo />
           </Grid>
         </Grid>
       </StyledContainerStudentDetails>
